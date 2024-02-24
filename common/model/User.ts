52fb0ce795482cbf4a67fa45059f689ebbe1b999
João Golias <joaovgolias@gmail.com>
2024-02-24 16:22:01 -0300

    updaing user model

--- common/model/User.ts
+++ common/model/User.ts
@@ -1,4 +1,8 @@
 export type User = {
     id: number
+    email: string
+    password: string
     name: string | undefined | null
+    birthdate: Date | undefined | null
+    gender: string | undefined | null
 }
